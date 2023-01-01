1e211434da4363bb5236803ba98526c986db9be4
copperscrewer <jadejassajay@gmail.com>
2023-01-02 01:48:57 +0530

    added stuff

--- web3commands.js
+++ web3commands.js
@@ -1,7 +1,9 @@
-//retrieve private variables
+\//retrieve private variables
 
 await contract2.methods.owner().call() //for view or pure functions
 await contract2.methods.init().send({from: player}) //for gas functions
+
+await contract2.methods.init( param1, param2,  {from: player, value: 1000}) //actually works
 
 > await web3.eth.getStorageAt('0x39E2C1570093218C35b4549667896bcc675aeC23',1)
 '0x1000000000000000000000000000000000000000000000000020000000000003'
