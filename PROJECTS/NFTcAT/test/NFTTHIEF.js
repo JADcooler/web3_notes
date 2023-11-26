2068e541124ff566a226cbb557a402d59b5eca62
JADcooler <jadejassajay@gmail.com>
2023-11-26 16:31:30 +0530

    Asd

--- PROJECTS/NFTcAT/test/NFTTHIEF.js
+++ PROJECTS/NFTcAT/test/NFTTHIEF.js
@@ -1,3 +1,5 @@
+// https://docs.ethers.org/v5/api/
+// checkout
 const 
 {
     expect, assert
@@ -8,6 +10,7 @@ const {
     time
     
   } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { EDIT_DISTANCE_THRESHOLD } = require("hardhat/internal/constants");
 //globals
 let nftThief
 
@@ -75,8 +78,9 @@ describe("Can set Arbitrary token uri", function()
         it("Should getBlockWithTransactions ", async function(){
 
             // x = await ethers.provider.getBlockWithTransactions(await time.latestBlock())
-            assert.fail("doesnt ", "work ", "bruh");
+            // assert.fail("doesnt ", "work ", "bruh");
             //fail
+            // todo with a live network
 
         })
 
@@ -87,10 +91,94 @@ describe("Can set Arbitrary token uri", function()
     context("ENS ", () => {
         it("Should getResolver", async function (){
 
-            resolver = await ethers.provider.lookupAddress("0x5555763613a12D8F3e73be831DFf8598089d3dCa");
-            console.log("[ENS] ", resolver)
+            // resolver = await ethers.provider.lookupAddress("0x6fC21092DA55B392b045eD78F4732bff3C580e2c");
+            // console.log("[ENS] ", resolver)
+            //todo after switching network
+        })
+        it("Should resolveName", async function (){
+
+            // resolver = await ethers.provider.resolveName("ricmoo.eth");
+            // console.log("[ENS] ", resolver)
+            //todo after switching network
+
+            //SKIPPED EnsResolver methods
+            // https://docs.ethers.org/v5/api/providers/provider/#EnsResolver
 
         })
     })
+
+    context("Logs Methods", () => {
+        it("Should getNetwork", async function (){
+
+            networkis =  await ethers.provider.getNetwork();
+            console.log("[NETWORK] ", networkis);
+
+        })
+
+        it("Should getBlockNumber", async function (){
+
+            networkis =  await ethers.provider.getBlockNumber();
+            console.log("[BLOCK] ", networkis);
+
+        })
+
+        it("Should getGasPrice", async function (){
+
+            networkis =  (await ethers.provider.getFeeData()).gasPrice;
+            console.log("[GAS PRICE] ", networkis);
+
+        })
+
+        it("Should getFeeData", async function (){
+
+            networkis =  await ethers.provider.getFeeData();
+            console.log("[FEE DATA] ", networkis);
+
+        })
+    })
+
+    context("Transactions Methods", () => {
+
+        it("Should call", async function(){
+
+            await ethers.provider.call({
+                to: await nftThief.getAddress()
+                ,data: "0xe0df5b6f000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000072"
+            })
+            
+            // 0xe0df5b6f
+            // 0000000000000000000000000000000000000000000000000000000000000020
+            // 0000000000000000000000000000000000000000000000000000000000000001
+            // 0000000000000000000000000000000000000000000000000000000000000072
+            
+
+
+            // Actual for a string call [Offset to actual data, length, actual data]
+            // 0xf5b30904
+            // 0000000000000000000000000000000000000000000000000000000000000020
+            // 0000000000000000000000000000000000000000000000000000000000000001
+            // 7300000000000000000000000000000000000000000000000000000000000000
+
+            // My mistake
+            // 0xe0df5b6f00000000000000000000000000000000000000000000000000000001
+            // 00000000000000000000000000000000000000000000000000000000000000e1
+
+            // and 
+
+            // 0xe0df5b6f000000000000000000000000000000000000000000000000000000
+            // 0000000000000000000000000000000000000000000000000000000000000001
+            // 0000000000000000000000000000000000000000000000000000000000000001
+
+            // So actually it would be 
+            // 0xe0df5b6f
+            // 0000000000000000000000000000000000000000000000000000000000000020
+            // 0000000000000000000000000000000000000000000000000000000000000001
+            // 0000000000000000000000000000000000000000000000000000000000000072 //ascii for d
+
+
+        })
+
+    })
+
 
 })
